fd04eeda42be8fdd6b72fad58ab50c08f5dc6975
Bruno Oliveira <brunoolv25@gmail.com>
2021-03-02 17:34:32 -0300

    inserir

--- class/Usuario.php
+++ class/Usuario.php
@@ -50,13 +50,9 @@ class Usuario {
 		));
 
 		if (count($results) > 0) {
+			$this->setData($results[0]);
 			
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			
 		}
 
 	}
@@ -92,13 +88,8 @@ public function login($login,$password){
 
 		if (count($results) > 0) {
 			
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
-		
+			
+			$this->setData($results[0]);
 
 		} else {
 
@@ -108,8 +99,55 @@ public function login($login,$password){
 
 	}
 
+	public function setData($data){
 
 
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+
+	}
+
+	public function insert (){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if (count($results) >0) {
+			$this->setData($results[0]);
+
+		}
+
+	}
+
+	public function update($login,$password){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+		
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_usuarios SET deslogin = LOGIN, dessenha = :PASSWORD WHERE idusuarios = :ID", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha(),
+			':ID'=>$this->getIdusuario()
+		));
+
+
+	}
+
+	public function __construct($login = "", $password = ""){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+	}
 
 	public function __toString(){
 		return json_encode(array(
